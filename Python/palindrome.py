301e31874fdeaa5a47c5ce98146a99397f84d612
eyyytran <ltatran94@gmail.com>
2022-06-07 21:40:38 -0400

    cleaning

--- Python/palindrome.py
+++ Python/palindrome.py
@@ -1,27 +1,21 @@
 #This function will determine if a word is a palindrome.
+def palindrome(word):
+    flipped = word[::-1]
+    return flipped.lower() == word.lower()
 
-# def palindrome(word):
-#     flipped = word[::-1]
-#     return flipped.lower() == word.lower()
-
-# print(
-#     palindrome('aa'),
-#     palindrome('abc')
-# )
- ###This function can reverse a word without the slice function
-# def palindrome(word):
-#     flipped = ''
-#     for char in word:
-#         flipped = char + flipped    
-#     return flipped.lower() == word.lower()
-# print(palindrome('turtle'))
-
+ #This function can reverse a word without the slice function
 def palindrome_v2(word):
-#This is the list() constructor function. 
-    #input = word
-    #chars = list(input)
+    flipped = ''
+    for char in word:
+        flipped = char + flipped #this concatenates the chars in from the end of the word   
+    return flipped.lower() == word.lower()
+
+#This function turns the word into a list and reverses the list
+def palindrome_v3(word):
     chars = list(word.lower())
     chars.reverse()
     return list(word.lower()) == chars
-    
+
+
+
 
--- Python/python_blockchain/blockchain.py
+++ Python/python_blockchain/blockchain.py
@@ -16,9 +16,12 @@ def get_user_input():
 tx_amount = get_user_input()
 add_value(tx_amount)
 
+# tx_amount = get_user_input()
+# add_value(last_transaction = get_last_blockchain_value(),
+#            transaction_amount = tx_amount)
+
 tx_amount = get_user_input()
-add_value(last_transaction = get_last_blockchain_value(),
-           transaction_amount = tx_amount)
+add_value(tx_amount, get_last_blockchain_value())
 
 tx_amount = get_user_input()
 add_value(tx_amount, get_last_blockchain_value())

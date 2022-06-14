38ff93c51648a96d323a93cf2ce37ac724f803e0
eyyytran <ltatran94@gmail.com>
2022-06-14 16:13:16 -0400

    Update fencingGame.py

--- Python/fencingGame.py
+++ Python/fencingGame.py
@@ -18,20 +18,28 @@ class Player:
 listOfOpponents = []
 
 class Opponent(Player):
-    def __init__(self, name, rating = '', speed=0, skill=0, point=0):
+    def __init__(self, name, rating = '', speed=10, skill=10, point=0):
         super().__init__(name, speed, skill, point)
         self.rating = rating
     def addOpponent(self):
         listOfOpponents.append(Opponent)
         print(listOfOpponents)
 
-szilagyi = Opponent('Aron Szilagyi', 'GOAT', 100,100)
-fox = Opponent('Chloe Fox-Gitomer', 'B', 70, 70)
-joe = Opponent('Joe Shmoe', 'Unrated', 0, 0)
+# szilagyi = Opponent('Aron Szilagyi', 'GOAT', 100,100)
+# fox = Opponent('Chloe Fox-Gitomer', 'B', 70, 70)
+# joe = Opponent('Joe Shmoe', 'Unrated', 0, 0)
 
-szilagyi.addOpponent()
-fox.addOpponent()
-joe.addOpponent()
+# szilagyi.addOpponent()
+# fox.addOpponent()
+# joe.addOpponent()
+
+def playerPoint():
+    player.points += 1
+    print('The score is:\n' + player.name + ' - ' + str(player.points) + '\n'+ opponent.name + ' - ' + str(opponent.points))  
+
+def opponentPoint():
+    opponent.points += 1
+    print('The score is:\n' + player.name + ' - ' + str(player.points) + '\n'+ opponent.name + ' - ' + str(opponent.points))  
 
 while game_running:
     titleborders()
@@ -54,15 +62,44 @@ while game_running:
 
         if player_choice == '1':
             if opponent_choice == 3:
-                player.points += 1
-                print('The score is:\n' + player.name + ' - ' + str(player.points) + '\n'+ opponent.name + ' - ' + str(opponent.points))  
-        elif player_choice == '2':
+                playerPoint()
+                if player.points == 5:
+                    player_won = True
+                    print(player.name + ' has won the bout.\nSalute and shake hands.') 
+            if opponent_choice == 2:
+                if player.speed >= opponent.skill:
+                    playerPoint()
+                    if player.points == 5:
+                        player_won = True
+                        print(player.name + ' has won the bout.\nSalute and shake hands.') 
+                if player.speed <= opponent.skill:
+                    opponentPoint()
+                    if opponent.points == 5:
+                        opponent_won = True
+                        print(opponent.name + ' has won the bout.\nSalute and shake hands.')
+            if opponent_choice == 1:
+                if player.speed >= opponent.speed:
+                    playerPoint()
+                    if player.points == 5:
+                        player_won = True
+                        print(player.name + ' has won the bout.\nSalute and shake hands.') 
+                if player.speed <= opponent.speed:
+                    opponentPoint()
+                    if opponent.points == 5:
+                        opponent_won = True
+                        print(opponent.name + ' has won the bout.\nSalute and shake hands.')
+
+        if player_choice == '2':
             pass
         elif player_choice == '3':
             pass
         elif player_choice == '4':
             new_bout = False    
-        else:
-            print('Invalid input - Try again with an integer (1-4).')
+        
+        elif player_won == True or opponent_won == True:
+            new_bout = False
+
+        # else:
+        #     print('Invalid input - Try again with an integer (1-4).')
 
     break

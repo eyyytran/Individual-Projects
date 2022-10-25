4ed31648a7dd2a07022fdd51ad37d0e1a93f36f3
eyyytran <ltatran94@gmail.com>
2022-10-25 15:57:21 -0400

    Update first-app.js

--- nodePracticum/first-app.js
+++ nodePracticum/first-app.js
@@ -1,13 +1,25 @@
-function Horse(name) {
-    this.name = console.log(name)
-    this.callHorse = function () {
-        console.log('Hi Bob')
-    }
+const person = {
+    name: 'Andrea',
+    sayName: function () {
+        console.log(this.name) //this refers to person
+    },
 }
 
-const myHorse = new Horse('Bob')
+person.sayName() //person is calling the function, so this = person
 
-myHorse.name
+const localSayName = person.sayName
+localSayName() //global context is calling the function, so this = global object
 
-const myHorseName = myHorse.name
-console.log(myHorseName)
+const boundSayName = person.sayName.bind(person) //ignore the context, when this is called, this = person
+boundSayName()
+
+//arrow functions can change the behavior of this
+
+const horse = {
+    name: 'Bob',
+    sayName: () => {
+        console.log(this.name) //to make this work, replace this.name with horse.name
+    },
+}
+
+horse.sayName() //the arrow function is defining 'this' as what it was at time of declaration, which ^ is this = global

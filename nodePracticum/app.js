e820060fdcd5888bafa69def4c31ced29a57f6f5
eyyytran <ltatran94@gmail.com>
2022-10-26 19:45:46 -0400

    request headers

--- nodePracticum/app.js
+++ nodePracticum/app.js
@@ -2,7 +2,10 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
     console.log(req)
-    // process.exit()
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>')
+    res.write('<head></head>')
+    res.write('</html>')
 })
 
 server.listen(3000)

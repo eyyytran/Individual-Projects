914e913b213bbd3bccc3563529ade4369ad49417
eyyytran <ltatran94@gmail.com>
2022-11-03 17:22:45 -0400

    cleaned up code - removed followers(uneeded)

--- codingPracticum/algos/makeTwitter.js
+++ codingPracticum/algos/makeTwitter.js
@@ -1,7 +1,6 @@
 var Twitter = function () {
     this.users = new Map()
-    this.tweets = new Map()
-    this.users.set(1, { followers: [], followees: [] })
+    this.tweets = []
 }
 
 /**
@@ -10,9 +9,9 @@ var Twitter = function () {
  * @return {void}
  */
 Twitter.prototype.postTweet = function (userId, tweetId) {
+    if (!this.users.has(userId)) this.users.set(userId, [])
     const tweet = { userId, tweetId }
-    this.tweets.set(this.tweets.size, tweet)
-    if (!this.users.has(userId)) this.users.set(userId, { followers: [], followees: [] })
+    this.tweets.unshift(tweet)
 }
 
 /**
@@ -21,19 +20,15 @@ Twitter.prototype.postTweet = function (userId, tweetId) {
  */
 Twitter.prototype.getNewsFeed = function (userId) {
     const usersTweets = []
-    const tweetsToDisplay = []
-    const { followers, followees } = this.users.get(userId)
+    const followees = this.users.get(userId)
 
-    this.tweets.forEach((tweet, timestamp) => {
-        if (tweet.userId === userId) usersTweets.push([timestamp, tweet.tweetId])
-        else if (followees.includes(tweet.userId)) usersTweets.push([timestamp, tweet.tweetId])
-    })
-
-    usersTweets.sort((a, b) => b[0] - a[0])
-    usersTweets
-        .slice(0, Math.min(usersTweets.length, 10))
-        .forEach(([timestamp, tweetId]) => tweetsToDisplay.push(tweetId))
-    return tweetsToDisplay
+    for (const tweet of this.tweets) {
+        if (usersTweets.length === 10) break
+        if (tweet.userId === userId) usersTweets.push(tweet.tweetId)
+        else if (followees.includes(tweet.userId)) usersTweets.push(tweet.tweetId)
+    }
+    usersTweets.slice(0, Math.min(usersTweets.length, 10))
+    return usersTweets
 }
 
 /**
@@ -43,23 +38,10 @@ Twitter.prototype.getNewsFeed = function (userId) {
  */
 Twitter.prototype.follow = function (followerId, followeeId) {
     if (!this.users.has(followerId)) {
-        this.users.set(followerId, { followers: [], followees: [followeeId] })
+        this.users.set(followerId, [followeeId])
     } else {
-        const { followers, followees } = this.users.get(followerId)
-
-        this.users.set(followerId, {
-            followers: followers,
-            followees: [...followees, followeeId],
-        })
-    }
-    if (!this.users.has(followeeId)) {
-        this.users.set(followeeId, { followers: [followerId], followees: [] })
-    } else {
-        const { followers, followees } = this.users.get(followeeId)
-        this.users.set(followeeId, {
-            followees: followees,
-            followers: [...followers, followerId],
-        })
+        const followees = this.users.get(followerId)
+        followees.push(followeeId)
     }
 }
 
@@ -69,25 +51,11 @@ Twitter.prototype.follow = function (followerId, followeeId) {
  * @return {void}
  */
 Twitter.prototype.unfollow = function (followerId, followeeId) {
-    if (!this.users.has(followerId) || !this.users.has(followeeId)) return
-    const { followers, followees } = this.users.get(followerId)
+    if (!this.users.has(followeeId)) return
+    const followees = this.users.get(followerId)
     const followeeIndex = followees.indexOf(followeeId)
     if (followeeIndex === -1) return
     followees.splice(followeeIndex, 1)
-    this.users.set(followerId, {
-        followers,
-        followees,
-    })
-
-    const userToBeUpdated = this.users.get(followeeId)
-    const followeesToBeUpdated = userToBeUpdated.followees
-    const followersToBeUpdated = userToBeUpdated.followers
-    const followerIndex = followersToBeUpdated.indexOf(followerId)
-    followersToBeUpdated.splice(followerIndex, 1)
-    this.users.set(followeeId, {
-        followers: followersToBeUpdated,
-        followees: followeesToBeUpdated,
-    })
 }
 
 const newTwitter = new Twitter()

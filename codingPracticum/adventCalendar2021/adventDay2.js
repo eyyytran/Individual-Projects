c61702a6ae9d2c7a8a7ff4ba3d93c77968553903
eyyytran <ltatran94@gmail.com>
2022-11-04 19:53:31 -0400

    clean data for day2

---
+++ codingPracticum/adventCalendar2021/adventDay2.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+
+fs.readFile('./puzzleInputs/rawData/day2Inputs.txt', 'utf-8', (error, data) => {
+    const cleanedData = []
+    const dataToArray = data.toString().split(/\r?\n/)
+    dataToArray.forEach(command => {
+        const [direction, numberOfSpaces] = command.split(' ')
+        cleanedData.push([direction, parseInt(numberOfSpaces)])
+    })
+    try {
+        fs.writeFileSync(
+            './puzzleInputs/cleanedData/cleanedDay2Inputs.json',
+            JSON.stringify(cleanedData, null, 1)
+        )
+        console.log('finished updating file')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+const puzzleInputs = require('./puzzleInputs/cleanedData/cleanedDay2Inputs.json')
+
+const testCase = [
+    ['forward', 5],
+    ['down', 5],
+    ['forward', 8],
+    ['up', 3],
+    ['down', 8],
+    ['forward', 2],
+]
+const expectedResult = 150

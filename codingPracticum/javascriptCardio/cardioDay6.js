380f4e3b883610a4db3913ecad458c9af75eb949
eyyytran <ltatran94@gmail.com>
2022-11-04 19:33:49 -0400

    sliding window part 2

--- codingPracticum/javascriptCardio/cardioDay6.js
+++ codingPracticum/javascriptCardio/cardioDay6.js
@@ -1,10 +1,25 @@
 // Given an array of sonar readings with depths from the sea floor, count the number of times a depth measurement imcreases from the previous measurement. There is no measurement before the first value.
 
-const testCase = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
-const expectedResult = 7
+const testCase = [607, 618, 618, 617, 647, 716, 769, 792]
+const expectedResult = 5
 const fs = require('fs')
 
-const puzzleInputs = []
+fs.readFile('./puzzleInputs/day1inputs.txt', 'utf8', (err, data) => {
+    const cleanedData = []
+    const dataToArray = data.toString().split(/\r?\n/)
+    dataToArray.forEach(string => cleanedData.push(parseInt(string)))
+    try {
+        fs.writeFileSync(
+            './puzzleInputs/cleanedDay1Inputs.json',
+            JSON.stringify(cleanedData, null, 4)
+        )
+        console.log('finished loading data')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+const puzzleInputs = require('./puzzleInputs/cleanedDay1Inputs.json')
 
 const calculateNumberOfDepthIncreases = arrayOfDepths => {
     let counter = 0
@@ -20,14 +35,25 @@ const calculateNumberOfDepthIncreases = arrayOfDepths => {
     return counter
 }
 
-fs.readFile('./puzzleInputs/day1inputs.txt', 'utf8', (err, data) => {
-    const dataToArray = data.toString().split(/\r?\n/)
-    console.log(calculateNumberOfDepthIncreases(dataToArray))
-})
+// console.log(calculateNumberOfDepthIncreases(puzzleInputs))
 
-// console.log(
-//     'input: ',
-//     calculateNumberOfDepthIncreases(testCase),
-//     'expected result:',
-//     expectedResult
-// )
+const calculateNumberOfWindowIncreases = arrayOfDepths => {
+    let [first, second, third] = [0, 1, 2]
+    let counter = 0
+    let previousSum = null
+    while (third < arrayOfDepths.length) {
+        const currentSum = arrayOfDepths[first] + arrayOfDepths[second] + arrayOfDepths[third]
+        if (previousSum) {
+            if (currentSum > previousSum) {
+                counter++
+            }
+        }
+        previousSum = currentSum
+        first = second
+        second = third
+        third++
+    }
+    return counter
+}
+
+console.log(calculateNumberOfWindowIncreases(puzzleInputs))
